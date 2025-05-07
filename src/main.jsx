75a3bcf908bb57d176fb3310902e1321044a919b
bhi116 <bhi116@myy.haaga-helia.fi>
2025-05-07 21:28:09 +0300

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,25 @@
 import {createRoot} from 'react-dom/client';
 import React from 'react';
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Customers from './components/Customers.jsx';
 import Trainings from './components/Trainings.jsx';
 import Error from './components/Error.jsx';
 import Home from './components/Home.jsx';
 import Calendar from './components/Calendar.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        element: <Home />,
-        index: true
-      },
-      {
-        path: "customers",            
-        element: <Customers />,
-      },
-      {
-        path: "trainings",
-        element: <Trainings />,
-      },
-      {
-        path: "calendar",
-        element: <Calendar />,
-      }
-    ]
-  }
-]);
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="trainings" element={<Trainings />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 );
